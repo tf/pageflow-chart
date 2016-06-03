2cb0072e7f664527ffe798aefc4def41cccdaceb
Tim Fischbach <tfischbach@codevise.de>
2016-06-03 10:09:07 +0200

    Do not abort js execution on exception in chart
    Since the scraped site concatenates all js files, an exception causes
    immediate halt of js inside the chart. The original chart with
    separate js files continues to execute the next file though.
    
    Wrap each file in try/catch block and only log errors to debug
    console.

--- app/jobs/pageflow/chart/scrape_site_job.rb
+++ app/jobs/pageflow/chart/scrape_site_job.rb
@@ -19,12 +19,17 @@ module Pageflow
             content_type: 'text/html'
           )
 
-          downloader.load_all(scraper.javascript_urls, extension: '.js', separator: "\n;") do |file|
-            scraped_site.javascript_file = file
+          downloader.load_all(scraper.javascript_urls,
+                              extension: '.js',
+                              before_each: begin_try_catch,
+                              after_each: end_try_catch) do |javascript_file|
+            scraped_site.javascript_file = javascript_file
           end
 
-          downloader.load_all(scraper.stylesheet_urls, extension: '.css', separator: "\n;") do |file|
-            scraped_site.stylesheet_file = file
+          downloader.load_all(scraper.stylesheet_urls,
+                              extension: '.css',
+                              separator: "\n;") do |stylesheet_file|
+            scraped_site.stylesheet_file = stylesheet_file
           end
         end
 
@@ -38,6 +43,14 @@ module Pageflow
       def self.perform_with_result(scraped_site, options = {})
         # This is were the downloader passed to `initialize` is created.
         new(Downloader.new(base_url: scraped_site.url)).perform(scraped_site)
+      end
+
+      def begin_try_catch
+        ";try {\n"
+      end
+
+      def end_try_catch
+        "\n} catch(e) { console.log('Datawrapper raised: ' + (e.message || e)); }\n"
       end
     end
 
