6beefd06312cc9b2d4d556413231b6a645645dab
Tim Fischbach <tfischbach@codevise.de>
2017-08-17 14:45:07 +0200

    Try to minimize changes in script tag order
    Backport of #39
    
    Some datawrapper documents have the form
    
        <head>
          <script>
            // Inline script with required setup for library-a
          </script>
          <script src="library-a.js></script>
          <script src="library-b.js></script>
          <script>
            // Usage of library-b
          </script>
        </head>
    
    We need to make sure that the combined script tag is inserted between
    the inline script tags. We therefore insert the combined script tag
    before the first script tag with a src attribute.

--- spec/support/html_fragment.rb
+++ spec/support/html_fragment.rb
@@ -10,4 +10,17 @@ class HtmlFragment
   def has_tag?(selector)
     !!document.at_css(selector)
   end
+
+  def has_tags_in_order?(*selectors)
+    all_elements = document.css('*')
+
+    elements = selectors.map do |selector|
+      document.at_css(selector) ||
+        raise("Selector '#{selector}}' did not match any element.")
+    end
+
+    elements.each_cons(2).all? do |element1, element2|
+      all_elements.index(element1) < all_elements.index(element2)
+    end
+  end
 end
