d5bd74d1d123491c937358a76e95f366065181a1
Tim Fischbach <tfischbach@codevise.de>
2019-10-10 17:51:13 +0200

    Make export/import integration test of scraped site work
    Exported attachments need to be present.

--- spec/integration/file_type_spec.rb
+++ spec/integration/file_type_spec.rb
@@ -5,6 +5,6 @@ module Pageflow
   module Chart
     Pageflow::Lint.file_type(:scraped_site,
                              create_file_type: -> { Chart.scraped_site_file_type },
-                             create_file: -> { create(:scraped_site) })
+                             create_file: -> { create(:scraped_site, :processed) })
   end
 end
